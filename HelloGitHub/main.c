730552dd19225fefd631575ec778768085f25143
Duy Huynh <dhuynh88@uw.edu>
2015-04-01 12:12:35 -0700

    Initial commit

---
+++ HelloGitHub/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 1, 2015
+ *      Author: dhuynh88
+ */
+
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello GitHub!");
+	return 1;
+}
---
+++ HelloWorld/main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 1, 2015
+ *      Author: dhuynh88
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello world!");
+	return 1;
+}
