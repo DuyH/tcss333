d87c4038e7e33389be267274d07919ba7434f328
Duy Huynh <dhuynh88@uw.edu>
2015-04-02 23:50:25 -0700

    Initial commit assignment 1 - binary calculator

---
+++ Assignment 1/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 2, 2015
+ *      Author: Duy
+ */
+
+#include <stdio.h>
+
+//functions
+
+//vars
+#define TRUE 1;
+#define FALSE 0;
+
+int main(void) {
+	char c;
+
+	scanf("%c", &c);
+
+	return 0;
+}
+
