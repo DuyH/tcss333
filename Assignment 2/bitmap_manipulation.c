2d56b2f56c059d533bd5de7d87a89fba8f18790c
Duy Huynh <dhuynh88@uw.edu>
2015-04-13 16:05:39 -0700

    Writing the file headers is correct. Except the forloops are not working so I am getting all black image outputs

--- Assignment 2/bitmap_manipulation.c
+++ Assignment 2/bitmap_manipulation.c
@@ -9,59 +9,67 @@
 
 int main(void) {
 
-    FILE *infile1 = fopen("in1.bmp", "rb"); // rb = readbinary
-    FILE *infile2 = fopen("in2.bmp", "rb"); // rb = readbinary
-    FILE *outfile1 = fopen("blend.bmp", "wb"); // wb  = write binary
-    FILE *outfile2 = fopen("checker.bmp", "wb"); // wb  = write binary
+	FILE *infile1 = fopen("in1.bmp", "rb"); // rb = readbinary
+	FILE *infile2 = fopen("in2.bmp", "rb"); // rb = readbinary
+	FILE *outfile1 = fopen("blend.bmp", "wb"); // wb  = write binary
+	FILE *outfile2 = fopen("checker.bmp", "wb"); // wb  = write binary
 
-    // Reading in the header of the first image (54 bytes):
-    char header1[18]; // bmp file header (before the file width)
-    fread(header1, 1, 34, infile1);
+	// Reading in the header of the first image (54 bytes):
+	char header1[18]; // bmp file header (before the file width)
+	fread(header1, 1, 18, infile1);
 
-    int width;
-    fread(&width, sizeof(int), 1, infile1);
+	int width;
+	fread(&width, sizeof(int), 1, infile1);
+	printf("Width: %d\n", width);
 
-    int height;
-    fread(&height, sizeof(int), 1, infile1);
+	int height;
+	fread(&height, sizeof(int), 1, infile1);
+	printf("Height: %d\n", height);
 
-    int imageSize;
-    fread(&imageSize, sizeof(int), 1, infile1);
+	char header2[8];
+	fread(header2, 1, 8, infile1);
 
-    char header2[16]; // bytes after the image size piece
-    fread(header2, 1, 16, infile1);
+	int imageSize;
+	fread(&imageSize, sizeof(int), 1, infile1);
+	printf("Image Size: %d\n", imageSize);
 
-    int pixelSize = width * height * 4;
+	char header3[16]; // bytes after the image size piece
+	fread(header3, 1, 16, infile1);
 
-    // Store pixel data of first image in pixelsIn1 2d-array
-    char pixelsIn1[width][height * 3]; //[rows][cols]
-    fread(pixelsIn1, 1, imageSize, infile1); // read in the image file
+	int pixelSize = width * height * 4;
 
-    // Store pixel data of second image in pixelsIn2 2d-array
-    char pixelsIn2[width][height * 3];
-    fread(pixelsIn2, 1, imageSize, infile2);
+	// Store pixel data of first image in pixelsIn1 2d-array
+	char pixelsIn1[width][height * 3]; //[rows][cols]
+	fread(pixelsIn1, 1, imageSize, infile1); // read in the image file
 
-    // A pixel array for blend
-    char pixelsOut1[width][height * 3];
+	// Store pixel data of second image in pixelsIn2 2d-array
+	char pixelsIn2[width][height * 3];
+	fread(pixelsIn2, 1, imageSize, infile2);
 
-    // A pixel array for checker
-    char pixelsOut2[width][height * 3];
+	// A pixel array for blend
+	char pixelsOut1[width][height * 3];
 
-    // Blend them pixels!
-    int r, c; // rows, columns
+	// A pixel array for checker
+	char pixelsOut2[width][height * 3];
 
-    for (r = 0; r < height; r++) {
-        for (c = 0; c < width * 3; c++) {
-            pixelsOut1[r][c] = (pixelsIn1[r][c] + pixelsIn2[r][c]) / 2;
-        }
-    }
+	// Blend them pixels!
+	int r, c; // rows, columns
 
-    // Write to outfile1 (blend.bmp)
-    fwrite(header1, 1, 34, outfile1);
-    fwrite(&width, sizeof(int), 1, outfile1);
-    fwrite(&height, sizeof(int), 1, outfile1);
-    fwrite(&imageSize, sizeof(int), 1, outfile1);
-    fwrite(header2, 1, 16, outfile1);
-    fwrite(pixelsOut1, 1, pixelSize, outfile1); // read in the image file
+	for (r = 0; r < height; r++) {
+		for (c = 0; c < width * 3; c++) {
+
+			pixelsOut1[r][c] = (pixelsIn1[r][c] + pixelsIn2[r][c]) / 2;
+		}
+	}
+
+	// Write to outfile1 (blend.bmp)
+	fwrite(header1, 1, 18, outfile1);
+	fwrite(&width, sizeof(int), 1, outfile1);
+	fwrite(&height, sizeof(int), 1, outfile1);
+	fwrite(header2, 1, 8, outfile1);
+	fwrite(&imageSize, sizeof(int), 1, outfile1);
+	fwrite(header3, 1, 16, outfile1);
+	fwrite(pixelsOut1, 1, pixelSize, outfile1); // read in the image file
 
 //	// Write to outfile2 (checkered.bmp)
 //	fwrite(header1, 1, 34, outfile1);
@@ -71,12 +79,12 @@ int main(void) {
 //	fwrite(header2, 1, 16, outfile1);
 //	fwrite(pixelsIn1, 1, 115200, outfile1); // read in the image file
 
-    fclose(infile1);
-    fclose(infile2);
+	fclose(infile1);
+	fclose(infile2);
 
-    fclose(outfile1);
-    fclose(outfile2);
+	fclose(outfile1);
+	fclose(outfile2);
 
-    return 0;
+	return 0;
 }
 
