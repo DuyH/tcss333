ccec9728a7f3e7d67766b55182260063948f5249
duyh <dhuynh88@uw.edu>
2015-04-15 00:05:23 -0700

    Finished the functional part. Both blend and checker bitmaps are being generated correctly. Need to comment and format for submssion.

--- Assignment 2/bitmap_manipulation.c
+++ Assignment 2/bitmap_manipulation.c
@@ -11,8 +11,8 @@ int main(void) {
 
     FILE *infile1 = fopen("in1.bmp", "rb"); // rb = readbinary
     FILE *infile2 = fopen("in2.bmp", "rb"); // rb = readbinary
-    FILE *outfile1 = fopen("blend.bmp", "wb"); // wb  = write binary
-    FILE *outfile2 = fopen("checker.bmp", "wb"); // wb  = write binary
+    FILE *blendFile = fopen("blend.bmp", "wb"); // wb  = write binary
+    FILE *checkerFile = fopen("checker.bmp", "wb"); // wb  = write binary
 
     // Reading in the header of the first image (54 bytes):
     char header1[18]; // bmp file header (before the file width)
@@ -41,40 +41,60 @@ int main(void) {
     fread(pixelsIn2, 1, imageSize, infile2);
 
     // A pixel array for blend
-    unsigned char blendArr[height][width * 3];
+    unsigned char blendPixArr[height][width * 3];
 
     // A pixel array for checker
-    unsigned char pixelsOut2[height][width * 3];
+    unsigned char checkerPixArr[height][width * 3];
 
-    // Blend them pixels!
+    // BLEND them pixels!
     int r, c; // rows, columns
 
     for (r = 0; r < height; r++) {
         for (c = 0; c < width * 3; c++) {
-            blendArr[r][c] = (pixelsIn1[r][c] + pixelsIn2[r][c]) / 2;
+            blendPixArr[r][c] = (pixelsIn1[r][c] + pixelsIn2[r][c]) / 2;
+        }
+    }
+
+    // CHECKER them pixels!
+
+    int flag = 0;
+
+    for (r = 0; r < height; r++) {
+        for (c = 0; c < width * 3; c++) {
+            if (c % (width * 3 / 8) == 0) {
+                flag++;
+            }
+            if (flag % height == 0) {
+                flag++;
+            }
+            if (flag % 2 == 0) {
+                checkerPixArr[r][c] = pixelsIn1[r][c];
+            } else {
+                checkerPixArr[r][c] = pixelsIn2[r][c];
+            }
+
         }
     }
 
     // Write to outfile1 (blend.bmp)
-    fwrite(header1, 1, 18, outfile1);
-    fwrite(&width, sizeof(int), 1, outfile1);
-    fwrite(&height, sizeof(int), 1, outfile1);
-    fwrite(header2, 1, 28, outfile1);
-    fwrite(blendArr, 1, imageSize, outfile1); // read in the image file
+    fwrite(header1, 1, 18, blendFile);
+    fwrite(&width, sizeof(int), 1, blendFile);
+    fwrite(&height, sizeof(int), 1, blendFile);
+    fwrite(header2, 1, 28, blendFile);
+    fwrite(blendPixArr, 1, imageSize, blendFile); // read in the image file
 
-//	// Write to outfile2 (checkered.bmp)
-//	fwrite(header1, 1, 34, outfile1);
-//	fwrite(&width, sizeof(int), 1, outfile1);
-//	fwrite(&height, sizeof(int), 1, outfile1);
-//	fwrite(&imageSize, sizeof(int), 1, outfile1);
-//	fwrite(header2, 1, 16, outfile1);
-//	fwrite(pixelsIn1, 1, 115200, outfile1); // read in the image file
+    // Write to outfile2 (checkered.bmp)
+    fwrite(header1, 1, 18, checkerFile);
+    fwrite(&width, sizeof(int), 1, checkerFile);
+    fwrite(&height, sizeof(int), 1, checkerFile);
+    fwrite(header2, 1, 28, checkerFile);
+    fwrite(checkerPixArr, 1, imageSize, checkerFile); // read in the image file
 
     fclose(infile1);
     fclose(infile2);
 
-    fclose(outfile1);
-    fclose(outfile2);
+    fclose(blendFile);
+    fclose(checkerFile);
 
     return 0;
 }
