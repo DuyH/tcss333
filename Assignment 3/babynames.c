47f3b85b5ba4a9069df3b23bcc0da92b320f99d2
duyh <dhuynh88@uw.edu>
2015-04-20 13:58:14 -0700

    Initial commit, assignment 3

---
+++ Assignment 3/babynames.c
@@ -0,0 +1,15 @@
+/*
+ * babynames.c
+ *
+ *  Created on: Apr 20, 2015
+ *      Author: duy
+ */
+
+#define MAX_NAME_LENGTH 100
+#define MAX_NAMES 100
+
+void readAll();
+void readFile();
+void processNameRank();
+void alphanbetize();
+void outputFile();
