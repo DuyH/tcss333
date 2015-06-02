ffbf8a1366fe420b4b7c6b7d1e66ee60396c2780
duyh <dhuynh88@uw.edu>
2015-06-02 16:51:31 -0700

    printing out the float.

--- Assignment 7/float.c
+++ Assignment 7/float.c
@@ -23,19 +23,22 @@ float getFloat() {
     float f;
     printf("Enter a float: ");
     scanf("%f", &f);
+    printf("%f\n", f);
+    printf("%d\n", f);
+    printf("%x\n", f);
     return f;
 }
 
 void printFloat(float f) {
 
     // Print number in float:
-    printf("Your float was read as: %f\n", f);
+    printf("Your float was read as: \n");
 
     // Print number in 32 bits:
     printf("Your float in 32 bits: ");
-    unsigned int bin;
+    unsigned int bin = f;
     int k;
-    for (k = 0; k < 32; k++) {
+    for (k = 0; k < 64; k++) {
         if (bin & 0x80000000) {
             printf("1");
         } else {
@@ -45,8 +48,20 @@ void printFloat(float f) {
     }
 
     // Print the sign:
-    printf("Sign: ");
-    if()
+    printf("\nSign: ");
+    unsigned int sign, expo, fraction;
+    bin = f;
+    sign = (bin & 0x80000000) ? 1 : 0;
+    printf("%d", sign);
+
+    // Printf the exponent:
+    printf("\nExponent: ");
+    for (k = 0; k < 8; k++) {
+        printf("%d", (bin & 0x40000000) ? 1 : 0);
+        bin = bin << 1;
+    }
+
+//Print the fraction:
 
 }
 
